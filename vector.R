3907bb2ce55d850e60b692cebaac278af6f88ac7
rutuja12balkundre <82512270+rutuja12balkundre@users.noreply.github.com>
2021-05-11 14:12:48 +0530

    Add files via upload

---
+++ vector.R
@@ -0,0 +1,38 @@
+vec1<- c(1,1.23,2.39)
+repet1 <- rep(10,5)
+seq2 <- seq(10,100, by = 10)
+
+x<- c(2,0,0,4)
+y <- c(1,9,9,9)
+
+sqrt(y)
+
+x[2]
+x[-4]
+
+x[1] <- 3
+x
+
+x[-1] = 5
+x
+y
+y < 9 
+
+
+Df_new<-data.frame(height=c(150,160),weight=c(65,72))
+Df_new
+View(Df_new)
+
+df<-data.frame(x=c(1,2,3),y=c('a','b','c'))
+df
+df[1,1]
+df[1,]
+df[,2]
+df[2,1,3]
+df[0,1:3]
+df[c(1,3),2]
+df[-2,2]
+df[c(1,3),1]
+df[-2,1]
+df[c(1,3),]
+df[-2,]
